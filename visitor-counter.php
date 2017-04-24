5dac7e934c5e622ad0fdd18398028791bb7fe989
Sanjib Narzary <ensilage@gmail.com>
2017-04-24 17:00:36 +0530

    changes

--- visitor-counter.php
+++ visitor-counter.php
@@ -1,25 +1,39 @@
 <?php
     session_start();
     $counter_name = "counter.txt";
+//var_dump(is_file($counter_name));
     // Check if a text file exists. If not create one and initialize it to zero.
     if (!file_exists($counter_name)) {
-        $f = fopen($counter_name, "w");
+        $f = fopen($counter_name, "w") or die('failed to open file');
         fwrite($f,"0");
+        fflush($f);
         fclose($f);
     }
     // Read the current value of our counter file
     $f = fopen($counter_name,"r");
-    $counterVal = fread($f, filesize($counter_name));
+    try {
+        $counterVal = fread($f, filesize($counter_name));
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+    fflush($f);
     fclose($f);
     // Has visitor been counted in this session?
     // If not, increase counter value by one
 
     if(!isset($_SESSION['hasVisited'])){
-        $_SESSION['hasVisited']="yes";
-        $counterVal = $counterVal+1;
-        $f = fopen($counter_name, "w");
-        fwrite($f, $counterVal);
+
+        try {
+            $_SESSION['hasVisited']="yes";
+            $counterVal = $counterVal+1;
+            $f = fopen($counter_name, "w") or die('failed to open file 2');
+            fwrite($f, $counterVal);
+        }
+        catch(Exception $e){
+            echo $e->getMessage();
+        }
+        fflush($f);
         fclose($f);
     }
-
     echo $counterVal;
