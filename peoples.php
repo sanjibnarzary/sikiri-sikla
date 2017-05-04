a9027afd53bd53f83e25bc59ee8bc64b463c0c61
Sanjib Narzary <ensilage@gmail.com>
2017-05-04 18:48:09 +0530

    changes

--- peoples.php
+++ peoples.php
@@ -97,6 +97,7 @@
                                         <?php echo empty($people['Degree'])?'':$people['Degree'].'<br>'?>
                                         <?php echo empty($people['Email'])?'':$people['Email'].'<br>'?>
                                         <?php echo empty($people['webURL'])?'':'<a href="'.$people['webURL'].'">'.$people['webURL'].'</a><br>'?>
+                                        <?php echo empty($people['Biodata'])?'':'<a style="text-decoration:none;" href="/upload/biodata/'.$people['Biodata'].'">CV</a>'.'<br>'?>
                                     </p>
                                 </td>
                             </tr>
@@ -123,6 +124,7 @@
                                     <?php echo empty($people['webURL'])?'':'<a href="'.$people['webURL'].'">'.$people['webURL'].'</a><br><br>'?>
 
                                     <?php echo empty($people['SubjectsTaught'])?'':$people['SubjectsTaught'].'<br>'?>
+                                    <?php echo empty($people['Biodata'])?'':'<a href="/upload/biodata/'.$people['Biodata'].'">Short CV</a>'.'<br>'?>
 
                                 </p>
                             </div>
---
+++ upload/biodata/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sn
+ * Date: 4/5/17
+ * Time: 6:30 PM
+ */
